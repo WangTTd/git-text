74f5c8bcf911c62e297f1e7e2ce78f1378814ead
wtt <wtt@atguigu.com>
2022-05-13 15:11:04 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello github!");
         System.out.println("hello github1!");
         System.out.println("hello github2!");
+        System.out.println("master test!");
+
     }
 }
