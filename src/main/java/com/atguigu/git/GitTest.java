c79497a897cc817adab882da15542bbae3f22c8d
wtt <wtt@atguigu.com>
2022-05-13 14:42:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wangtt
+ * @creat 2022-05-13-14:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
